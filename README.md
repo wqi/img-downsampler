e0c85a3018c2b57a4a6ef960bb48d3f6fb1f0199
William Qi <hello@williamqi.com>
2017-10-16 03:04:01 -0700

    downsampler v1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # img-downsampler
-Simple tool to automatically downsample images at different sizes
+Simple utility written in Python to automatically downsample images at different scales (currently 2x, 3x, and 4x) using OpenCV's implementation of bicubic interpolation. This utility can be especially useful in generating training and testing data sets for super-resolution tasks.
+
+## Requirements
+* Python 3
+* `opencv-python`
+
+## Usage
+
+```
+usage: downsample.py [-h] [-k] hr_img_dir lr_img_dir
+
+Downsize images at 2x, 3x, and 4x using bicubic interpolation.
+
+positional arguments:
+  hr_img_dir      path to high resolution image dir
+  lr_img_dir      path to desired output dir for downsampled images
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -k, --keepdims  keep original image dimensions in downsampled images
+```
---
+++ downsample.py
@@ -0,0 +1,59 @@
+import os
+import argparse
+import cv2
+
+
+# Parse args
+parser = argparse.ArgumentParser(description='Downsize images at 2x, 3x, and 4x\
+    using bicubic interpolation.')
+parser.add_argument("hr_img_dir", help="path to high resolution image dir")
+parser.add_argument("lr_img_dir", help="path to desired output dir for\
+    downsampled images")
+parser.add_argument("-k", "--keepdims", help="keep original image dimensions in\
+    downsampled images", action="store_true")
+args = parser.parse_args()
+
+hr_image_dir = args.hr_img_dir
+lr_image_dir = args.lr_img_dir
+
+# Create LR image dirs
+os.makedirs(lr_image_dir + "/2x", exist_ok=True)
+os.makedirs(lr_image_dir + "/3x", exist_ok=True)
+os.makedirs(lr_image_dir + "/4x", exist_ok=True)
+
+supported_img_formats = (".bmp", ".dib", ".jpeg", ".jpg", ".jpe", ".jp2",
+                         ".png", ".pbm", ".pgm", ".ppm", ".sr", ".ras", ".tif",
+                         ".tiff")
+
+# Downsample HR images
+for filename in os.listdir(hr_image_dir):
+    if not filename.endswith(supported_img_formats):
+        continue
+
+    # Read HR image
+    hr_img = cv2.imread(os.path.join(hr_image_dir, filename))
+    hr_img_dims = (hr_img.shape[1], hr_img.shape[0])
+
+    # Downsample image 2x
+    lr_img_2x = cv2.resize(hr_img, (0, 0), fx=0.5, fy=0.5,
+                           interpolation=cv2.INTER_CUBIC)
+    if args.keepdims:
+        lr_img_2x = cv2.resize(lr_img_2x, hr_img_dims,
+                               interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join(lr_image_dir + "/2x", filename), lr_img_2x)
+
+    # Downsample image 3x
+    lr_img_3x = cv2.resize(hr_img, (0, 0), fx=(1 / 3), fy=(1 / 3),
+                           interpolation=cv2.INTER_CUBIC)
+    if args.keepdims:
+        lr_img_3x = cv2.resize(lr_img_3x, hr_img_dims,
+                               interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join(lr_image_dir + "/3x", filename), lr_img_3x)
+
+    # Downsample image 4x
+    lr_img_4x = cv2.resize(hr_img, (0, 0), fx=0.25, fy=0.25,
+                           interpolation=cv2.INTER_CUBIC)
+    if args.keepdims:
+        lr_img_4x = cv2.resize(lr_img_4x, hr_img_dims,
+                               interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join(lr_image_dir + "/4x", filename), lr_img_4x)
